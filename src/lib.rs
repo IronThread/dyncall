5536ea71b64b3ffb3569b26cc1b4efb8d2c5ec72
Dang <zeroed32@protonmail.com>
2021-02-11 14:52:10 -0300

    fixed link

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
 //! Crate with the purporse of using more easely and powerfull the functionality of the C library
-//! [`dyncall`][https://www.dyncall.org]. Refer to the page for platforms supported.
+//! [`dyncall`](https://www.dyncall.org). Refer to the page for platforms supported.
 //! 
 //! Python have to be installed and in `PATH` to use this library at the moment.
 
